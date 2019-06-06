db42be8a6682595c0ad86cab97d1f1ad3479e403
lukastheober <oberthuer.lukas@gmail.com>
2019-06-06 15:19:31 +0200

    implemented ListCreationWizard

--- src/todo/ListCreationWizard.java
+++ src/todo/ListCreationWizard.java
@@ -1,19 +1,11 @@
 package todo;
 
-import javax.swing.JDialog;
 import java.awt.GridLayout;
-import java.util.Properties;
- 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
-import org.jdatepicker.impl.DateComponentFormatter;
-import org.jdatepicker.impl.JDatePanelImpl;
-import org.jdatepicker.impl.JDatePickerImpl;
-import org.jdatepicker.impl.UtilDateModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author benja
@@ -23,29 +15,22 @@ import org.jdatepicker.impl.UtilDateModel;
 public class ListCreationWizard extends MyDialog {
 
 	private JTextField name;
-	private JDatePickerImpl date;
-	private JComboBox<String> colour;
-	private JComboBox<String> interval;
 	private JButton cancel;
 	private JButton createTask;
 
-    private String[] colours = { "Auswählen", "Blau", "Rot", "Grün" };
-    private String[] intervals = { "Auswählen", "1 h", "2 h", "3 h", "4 h", "5 h" };
-	
-	// CONSTRUCTOR
-
 	public ListCreationWizard(Controller controller) {
 		super(controller);
-		// TODO Auto-generated constructor stub
+		initialize();
 	}
-	
+
 	private void initialize() {
 
+		// feel free to make it pretty
 		setLayout(new GridLayout(0, 2, 10, 10));
 		setSize(300, 400);
 		setTitle("Liste erstellen");
 		setLocationRelativeTo(null);
-		//setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		initializeFields();
 		setVisible(true);
 		pack();
@@ -59,37 +44,30 @@ public class ListCreationWizard extends MyDialog {
 		name.setName("name");
 		this.add(name);
 
-		this.add(new JLabel("    Ablaufdatum"));
-		Properties p = new Properties();
-		p.put("text.today", "today");
-		p.put("text.month", "month");
-		p.put("text.year", "year");
-		UtilDateModel model = new UtilDateModel();
-		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
-		date = new JDatePickerImpl(datePanel, new DateComponentFormatter());
-		date.getJFormattedTextField().setText("06.06.2019");
-		this.add(date);
-
-		this.add(new JLabel("    Farbe"));
-		colour = new JComboBox<String>(colours);
-		colour.setName("colour");
-		this.add(colour);
-
-		this.add(new JLabel("    Intervall"));
-		interval = new JComboBox<String>(intervals);
-		interval.setName("interval");
-		this.add(interval);
-
 		cancel = new JButton("  Abbrechen");
 		cancel.setName("cancel");
+		cancel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+
+		});
 		this.add(cancel);
 
 		createTask = new JButton("  Aufgabe erstellen");
 		createTask.setName("create");
+		createTask.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				// missing Constructor with params
+				System.out.println("Missing ListOfTasks Constructor, doing nothing");
+				controller.addList(new ListOfTasks());
+			}
+		});
 		this.add(createTask);
-
 	}
-
-
-
 }
