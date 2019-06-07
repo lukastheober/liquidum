b72bf1d383f90ea80aab12766323e54e967928c4
1826914 <1826914@hs-mannheim.de>
2019-06-07 12:44:29 +0200

    GUI kinda finished? ListOfTasks almost finished

--- src/todo/GUI.java
+++ src/todo/GUI.java
@@ -1,5 +1,9 @@
 package todo;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
@@ -9,20 +13,38 @@ import javax.swing.JFrame;
  * @created 18-Mai-2019 16:01:12
  */
 public class GUI extends JFrame {
-
-	private ListContainer listContainer;
-	private MainMenuBar menuBar;
+	
+	private ListContainer listContainer = new ListContainer();
+	private MainMenuBar menuBar = new MainMenuBar();
 	
 	public GUI() {
-
+		JFrame guiFrame = new JFrame("Liquidum");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int height = screenSize.height;
+		int width = screenSize.width;
+		
+		//fullscreen
+		guiFrame.setSize(height, width);
+		
+		//charming padding(9/10screen)
+//		height = (height/10*9);
+//		width = (width/10*9);
+//		guiFrame.setSize(height, width);
+		
+		guiFrame.setJMenuBar(menuBar);
+		guiFrame.setContentPane(listContainer);
+		
+		guiFrame.setVisible(true);
 	}
-
+	public static void main(String[] args) {
+		new GUI();
+	}
 	/**
 	 * Updates the whole UI.
 	 * JComponents can be updated using updateUI().
 	 */
 	public void update(){
-
+		menuBar.updateUI();
+		listContainer.updateUI();
 	}
-
 }
--- src/todo/ListOfTasks.java
+++ src/todo/ListOfTasks.java
@@ -1,5 +1,7 @@
 package todo;
 
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.util.LinkedList;
 
 import javax.swing.*;
@@ -16,21 +18,53 @@ public class ListOfTasks extends JPanel {
 	private JButton addTaskButton;
 	private JButton deleteButton;
 	private JButton editButton;
+	//TODO is this important?
 	private JLabel nameLabel;
-	private ListSortMenu sortingMenu;
+	private ListSortMenu sortingMenu = new ListSortMenu();
 
 	public ListOfTasks(){
-
+		//test
+		addTaskButton = new JButton("+");
+		editButton = new JButton("Bearbeiten");
+		deleteButton = new JButton("X");
+		//testend
+		
+        JFrame listFrame = new JFrame(name);
+		JPanel topPanel = new JPanel();
+		JPanel workPanel = new JPanel();
+		JPanel gridPanel = new JPanel();
+//		topPanel.add(nameLabel);
+		topPanel.add(sortingMenu);
+		listFrame.add(topPanel);
+				
+		gridPanel.add(addTaskButton);
+		gridPanel.add(editButton);
+		gridPanel.add(deleteButton);
+		
+		workPanel.add(gridPanel);
+		listFrame.add(workPanel);
+			
+		listFrame.pack();
+		
+		listFrame.setSize(420, 666);
+		listFrame.setLocationRelativeTo(null);
+		listFrame.setVisible(true);
 	}
-
+	
+	public String getListName() {
+		return this.name;
+	}
+	
+	public static void main(String[] args) {
+		new ListOfTasks();
+	}
 	/**
 	 * Overwrites all data in this List with data of the new Task, except tasks.
 	 * 
 	 * @param newList
 	 */
 	public void overwrite(ListOfTasks newList){
-
+		this.name = newList.name;
+		this.nameLabel = newList.nameLabel;
 	}
-
-
 }
